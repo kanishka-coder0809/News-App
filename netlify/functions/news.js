abdc26ebb0f62413788913e15a08f723cffa4079
Kanishka Thakur <kanishkathakur710@gmail.com>
2025-08-18 20:50:33 +0530

    commit script.js

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -1,13 +1,9 @@
-import fetch from "node-fetch"; // if Node <18, otherwise remove this line
-
 export async function handler(event) {
   try {
     const query = event.queryStringParameters.q || "India";
-
     const res = await fetch(
       `https://newsapi.org/v2/everything?q=${query}&apiKey=${process.env.NEWS_API_KEY}`
     );
-
     const data = await res.json();
 
     return {
